b50aec5587aab24a8c45abe9df9a9b5b46850910
piper <Aleksandr.Piper@mvideo.ru>
2024-10-25 11:40:36 +0000

    chore(all): fast change commit 2024.10.25 11:40:36
    body:

---
+++ bash/helm.sh
@@ -0,0 +1,26 @@
+
+function fhelm-roll-back-chart-by-grep-from-history () {
+
+    APP=${1:?"Error. You must supply helm release name."}
+    GREP_STR=${2:?"Error. You must supply a grep string to select revision."}
+
+    helm history ${APP} | grep -v "${GREP_STR}" | while read rrev stub; do echo $rrev $stub; echo $rrev; let rrev--; echo $rrev; helm rollback ${APP} $rrev; done
+}
+
+
+function fhelm-roll-back-last () {
+
+    APP=${1:?"Error. You must supply helm release name."}
+
+    helm history ${APP} | tail -n 1 | while read rrev stub; do echo $rrev $stub; echo $rrev; let rrev--; echo $rrev; helm rollback ${APP} $rrev; done
+}
+
+
+
+function fhelm () {
+    helm $@ $(  helm list -a | 
+                fzf | 
+                while read x1 stuff; do echo $x1; done
+            )
+}
+
