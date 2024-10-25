7bc08346184cc4a8746a34290e2a0dbf76209054
piper <Aleksandr.Piper@mvideo.ru>
2024-10-25 11:55:34 +0000

    chore(all): fast change commit 2024.10.25 11:55:34
    body:

---
+++ bash/aaabc-systool-fzf.sh
@@ -0,0 +1,3 @@
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border --inline-info'
---
+++ bash/aaabc-systool-kubectl.sh
@@ -0,0 +1,14 @@
+
+
+
+source <(kubectl completion bash)
+
+alias kc='kubectl'
+complete -F __start_kubectl kc
+source <(helm completion bash)
+
+function fkc-gen-full-config-from-splited-configs () {
+    export KUBECONFIGS_DIR="/home/blobby/.kube/splited_kubeconfigs/configs/"
+    export KUBECONFIG=$(find ${KUBECONFIGS_DIR} -type f | tr '\n' ':')
+    kubectl config view --flatten > ~/.kube/config
+}
