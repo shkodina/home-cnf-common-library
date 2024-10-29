7472320b75fbeb3c9dd4596d2f8c9f5981e03e29
piper <Aleksandr.Piper@mvideo.ru>
2024-10-29 06:42:35 +0000

    chore(all): fast change commit 2024.10.29 06:42:35
    body:
    bash/common-first/aaabc-systool-kubectl.sh

--- bash/common-first/aaabc-systool-kubectl.sh
+++ bash/common-first/aaabc-systool-kubectl.sh
@@ -8,7 +8,7 @@ complete -F __start_kubectl kc
 source <(helm completion bash)
 
 function fkc-gen-full-config-from-splited-configs () {
-    export KUBECONFIGS_DIR="/home/blobby/.kube/splited_kubeconfigs/configs/"
+    export KUBECONFIGS_DIR="~/.kube/splited_kubeconfigs/configs/"
     export KUBECONFIG=$(find ${KUBECONFIGS_DIR} -type f | tr '\n' ':')
     kubectl config view --flatten > ~/.kube/config
 }
