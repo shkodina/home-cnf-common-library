40bdbe21c08ac526a466ba3631361c256245da4a
piper <Aleksandr.Piper@mvideo.ru>
2025-01-27 14:57:24 +0000

    chore(all): fast change commit 2025.01.27 14:57:24
    body:
    bash/common-first/configs-editors.sh

--- bash/common-first/configs-editors.sh
+++ bash/common-first/configs-editors.sh
@@ -5,3 +5,12 @@ function fcnf-add-data-after-mask-from-file-in-file () {
 
     sed -i "/$mask/ r $data_file" $cnf_file
 }
+
+function fcnf-add-data-before-mask-from-file-in-file () {
+    local mask=$1
+    local data_file=$2
+    local cnf_file=$3
+
+    sed -i "/$mask/ e cat $data_file" $cnf_file
+}
+
