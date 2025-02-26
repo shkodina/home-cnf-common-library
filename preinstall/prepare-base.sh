739b942a0210d760252f7a2620982858df84dbb6
piper <Aleksandr.Piper@mvideo.ru>
2025-02-26 14:16:57 +0000

    chore(all): fast change commit 2025.02.26 14:16:57
    body:

---
+++ preinstall/prepare-base.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "apt install #################################################################################" \
+    && apt update && apt upgrade -y \
+    && apt install -y \
+        curl \
+        jq \
+        wget \
+        unzip \
+        uuid-runtime \
+        gettext-base \
+        expect-dev \
+        vim \
+        git \
+        netcat-openbsd \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -f /var/cache/apt/archives/*.deb
+
+echo "install yq  ##################################################################################" \
+    && wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 \
+    && chmod a+x /usr/local/bin/yq
