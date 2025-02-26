620727ce6626467adb4665f5b68b5e9971999a14
piper <Aleksandr.Piper@mvideo.ru>
2025-02-26 10:12:02 +0000

    chore(all): fast change commit 2025.02.26 10:12:02
    body:

--- bash/common-first/aaaaa-000-export-vars--base.sh
+++ bash/common-first/aaaaa-000-export-vars--base.sh
@@ -1,3 +1,4 @@
 export ANSIBLE_VAULT_PASSWORD_FILE=~/.ansible/vaults/deployer
 export SELF_STORE_KEY_FILE=~/.etc/self-stor-key-file
 export HOME_CNF_ETC_DIR=~/home_cnf/init.scripts
+export HOME_CNF_LIBRARY__ALL_RAW__URL="https://raw.githubusercontent.com/shkodina/home-cnf-common-library/refs/heads/main/all.sh"
