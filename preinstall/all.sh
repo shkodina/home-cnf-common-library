ed6083b83e9f9c17a3e446fd5dc2d4b20dd379ba
piper <Aleksandr.Piper@mvideo.ru>
2025-02-26 14:31:07 +0000

    chore(all): fast change commit 2025.02.26 14:31:07
    body:
    preinstall/all.sh

--- preinstall/all.sh
+++ preinstall/all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 >&2 echo "
-apt update && apt install -y curl && curl https://raw.githubusercontent.com/shkodina/home-cnf-common-library/refs/heads/main/preinstall/all.sh | bash
+apt update ; apt install -y curl ; curl https://raw.githubusercontent.com/shkodina/home-cnf-common-library/refs/heads/main/preinstall/all.sh | bash
 "
 
 url_base=https://raw.githubusercontent.com/shkodina/home-cnf-common-library/refs/heads/main/preinstall
