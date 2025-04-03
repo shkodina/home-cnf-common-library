7585895d28aa6b623e2a8e10372ad2e0dbd20a29
piper.aa <piper.aa@lime-shop.com>
2025-04-03 02:51:07 +0000

    chore(all): fast change commit 2025.04.03 02:51:06
    body:

--- all.sh
+++ all.sh
@@ -1616,6 +1616,27 @@ function fvault-cli-get-value-from-root () {  #  $1 path  $2 key(field)
         vault kv get -field=$f $1$2
     }
 }
+function yc-activate-profile () {
+    local profile=$(yc config profile list | cut -d ' ' -f1 | fzf)
+    yc config profile activate ${profile}
+    sed -i "/YC_TOKEN=/d" ~/.bashrc
+    export YC_TOKEN=$(yc iam create-token)
+    export YC_CLOUD_ID=$(yc config get cloud-id)
+    export YC_FOLDER_ID=$(yc config get folder-id)
+}
+alias fyc-profile-activate=yc-activate-profile
+function fyc-set-external-yc-token () {
+    sed -i "/YC_TOKEN=/d" ~/.bashrc
+    echo export YC_TOKEN=$1 | tee -a ~/.bashrc
+}
+function fyc-get-vmid () {
+    local vm=$(yc compute instance list --format json | jq -r .[].name | fzf)
+    local vmid=$(yc compute instance get --name $vm --format json | jq -r .id)
+    echo $vmid
+}
+function fyc-ssh () {
+    yc compute ssh   --id $(fyc-get-vmid)   --identity-file ~/.ssh/id_rsa
+}
    ###    ##       ####    ###     ######  ########  ######
   ## ##   ##        ##    ## ##   ##    ## ##       ##    ##
  ##   ##  ##        ##   ##   ##  ##       ##       ##
