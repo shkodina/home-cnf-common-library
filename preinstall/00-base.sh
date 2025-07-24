91abfc1067691acd3145955cacb90ed765aa1c0a
piper.aa <piper.aa@lime-shop.com>
2025-07-24 08:40:15 +0300

    chore(all): fast change commit 2025.07.24 08:40:15
    body:
    preinstall/00-base.sh

--- preinstall/00-base.sh
+++ preinstall/00-base.sh
@@ -16,7 +16,10 @@ echo "apt install ##############################################################
         git \
         netcat-openbsd \
     && rm -rf /var/lib/apt/lists/* \
-    && rm -f /var/cache/apt/archives/*.deb
+    && rm -f /var/cache/apt/archives/*.deb 
+
+echo "Set Time-Zone #################################################################################" \
+    && timedatectl set-timezone Europe/Moscow
 
 echo "install yq  ##################################################################################" \
     && wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 \
