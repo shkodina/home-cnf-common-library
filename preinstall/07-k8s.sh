d76e4c398b86d2c323bae27006faa3a682ec6aa1
piper.aa <piper.aa@lime-shop.com>
2025-07-31 08:59:30 +0300

    chore(all): fast change commit 2025.07.31 08:59:29
    body:
    preinstall/07-k8s.sh

--- preinstall/07-k8s.sh
+++ preinstall/07-k8s.sh
@@ -38,3 +38,13 @@ echo "update bashrc  ###########################################################
 echo "install yandex cloud cli = yc  ##############################################################################" \
     && curl -fsSL storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
 
+echo "install krew  ##############################################################################" && \
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
