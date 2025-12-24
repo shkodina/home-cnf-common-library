922394ac9ecdd47a1c6ad6ccbd1df77b5aea7792
piper.aa <piper.aa@lime-shop.com>
2025-12-24 12:40:22 +0300

    chore(all): fast change commit 2025.12.24 12:40:22
    body:
    bash/common-first/yc.sh

--- all.sh
+++ all.sh
@@ -1709,14 +1709,6 @@ function fyc-set-external-yc-token () {
     sed -i "/YC_TOKEN=/d" ~/.bashrc
     echo export YC_TOKEN=$1 | tee -a ~/.bashrc
 }
-function fyc-get-vmid () {
-    local vm=$(yc compute instance list --format json | jq -r .[].name | fzf)
-    local vmid=$(yc compute instance get --name $vm --format json | jq -r .id)
-    echo $vmid
-}
-function fyc-ssh () {
-    yc compute ssh   --id $(fyc-get-vmid)   --identity-file ~/.ssh/id_rsa
-}
 function fyc-subnet-list () {
     yc vpc subnet list --format json | yq .[].name
 }
