4b97a2bc5a3c07f4d772e00229dc70819f76274f
piper.aa <piper.aa@lime-shop.com>
2026-02-06 11:58:34 +0300

    chore(all): fast change commit 2026.02.06 11:58:34
    body:
    bash/common-first/ldapsearch.sh

--- all.sh
+++ all.sh
@@ -1511,6 +1511,16 @@ function fldap-search-user () {  #  $1=user.name
     -H "ldap://${LDAPSEARCH__H}" \
     -s sub "(&(objectClass=user)(sAMAccountName=$1))"
 }
+function fldap-search-user-last-pwd-set-date-time () {  #  $1=user.name
+local ts=$(fldap-search-user $1 | grep pwdLastSet: | cut -d: -f2- | tr -d ' ' )
+python3 - << EOF
+import datetime
+ft = ${ts}
+unix = (ft - 116444736000000000) / 10_000_000
+dt = datetime.datetime.fromtimestamp(unix, datetime.UTC)
+print(dt)
+EOF
+}
 function f_log_title () {
     echo ""
     echo -e "${CLR_GREEN}#################################################"
